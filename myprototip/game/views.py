78f063f6261d4766e48b26addb62bbbbf701742b
frau-muller <frau-muller@tut.by>
2021-10-10 20:44:08 +0300

    urls

--- myprototip/game/views.py
+++ myprototip/game/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def indexx(request):
+    #print(request)
+    return HttpResponse('<h1>Hello world</h1>')
+
+def test(request):
+    return HttpResponse('<h1>Test page<h/1>')
