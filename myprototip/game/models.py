b2de8b464a98cbff5fac6828f73b77f382f1efb9
frau-muller <frau-muller@tut.by>
2021-10-11 00:46:08 +0300

    crud

--- myprototip/game/models.py
+++ myprototip/game/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+
+    CHOICES_ANSWER = (
+        ('a', 'A'),
+        ('b', 'B'),
+        ('c', 'C'),
+        ('d', 'D'),
+    )
+    question = models.TextField(verbose_name="Вопрос")
+    option_a = models.CharField(max_length=250, verbose_name="Выбор A")
+    option_b = models.CharField(max_length=250, verbose_name="Выбор B")
+    option_c = models.CharField(max_length=250, verbose_name="Выбор C")
+    option_d = models.CharField(max_length=250, verbose_name="Выбор D")
+    correct_answer = models.CharField(max_length=5, choices=CHOICES_ANSWER, verbose_name="Правильный ответ")
